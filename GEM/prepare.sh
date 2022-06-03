4d47922c85a579ccee253992c6db33ee016450d2
CarlosAlbertoCalvachiSalas <98483297+CarlosAlbertoCalvachiSalas@users.noreply.github.com>
2022-06-02 23:55:32 -0500

    Add files via upload

---
+++ GEM/prepare.sh
@@ -0,0 +1,64 @@
+cd 380
+cmake .
+make  
+cd ..
+
+cd 390
+cmake .
+make  
+cd ..
+
+cd 400
+cmake .
+make  
+cd ..
+
+cd 410
+cmake .
+make  
+cd ..
+
+cd 420
+cmake .
+make  
+cd ..
+
+cd 430
+cmake .
+make  
+cd ..
+
+cd 440
+cmake .
+make  
+cd ..
+
+cd 450
+cmake .
+make  
+cd ..
+
+cd 460
+cmake .
+make  
+cd ..
+
+cd 470
+cmake .
+make  
+cd ..
+
+cd 480
+cmake .
+make  
+cd ..
+
+cd 490
+cmake .
+make  
+cd ..
+
+cd 500
+cmake .
+make  
+cd ..
